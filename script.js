26701ed3267def55bf6c337c7194b5b8c1e8265d
rodgers <rodgerstarrodgers@gmail.com>
2025-01-05 03:19:43 +0300

    operators in js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-console.log('hello world')
-console.log('rodgers')
-
-// variables
-name = "rodgers"
-console.log(name)
+// console.log('hello world')
+// console.log('rodgers')
+//
+// // variables
+// name = "rodgers"
+// console.log(name)
 
 // operators
+// food = 200
+// tipPercentage = 0.2
+// tipAmount = food * tipPercentage
+//
+// console.log(tipAmount)
+
+// user input
+food = prompt('what is your favourite food?')
+
+co
