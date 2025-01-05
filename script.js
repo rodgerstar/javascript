705fe52952542e07b62fc0fc62e22c731878c4a7
rodgers <rodgerstarrodgers@gmail.com>
2025-01-05 03:44:15 +0300

    user input

--- script.js
+++ script.js
@@ -11,8 +11,16 @@
 // tipAmount = food * tipPercentage
 //
 // console.log(tipAmount)
-
+// Import the prompt-sync module
+const prompt = require('prompt-sync')();
 // user input
-food = prompt('what is your favourite food?')
+food = Number(prompt('How much was the  food?'))
+tipPercentage = Number(prompt('tip %?') / 100)
+tipAmount = food * tipPercentage
 
-co
+total = food + tipAmount
+
+console.log('tip Amount:', tipAmount)
+console.log('total:', total)
+
+// data types (strings , numbers)
