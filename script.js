b02a1917d9acd7fd985396cb49c32d2c3be56655
rodgers <rodgerstarrodgers@gmail.com>
2025-01-05 04:06:24 +0300

    Math.random and Math.floor functions

--- script.js
+++ script.js
@@ -12,15 +12,25 @@
 //
 // console.log(tipAmount)
 // Import the prompt-sync module
-const prompt = require('prompt-sync')();
-// user input
-food = Number(prompt('How much was the  food?'))
-tipPercentage = Number(prompt('tip %?') / 100)
-tipAmount = food * tipPercentage
+// const prompt = require('prompt-sync')();
+// // user input
+// food = Number(prompt('How much was the  food?'))
+// tipPercentage = Number(prompt('tip %?') / 100)
+// tipAmount = food * tipPercentage
+// tipAmount = Math.floor(tipAmount)
+//
+// total = food + tipAmount
+//
+// console.log('tip Amount:', tipAmount)
+// console.log('total:', total)
 
-total = food + tipAmount
+// data types (
+// strings 'jane', 'hello'
+//  numbers 1,2,3,4,5,67
+//  arrays []
+//  objects {}
+//  boolean  true / false )
 
-console.log('tip Amount:', tipAmount)
-console.log('total:', total)
-
-// data types (strings , numbers)
+number = Math.random() * 3
+number = Math.floor(number)
+console.log(number)
