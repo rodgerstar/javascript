bd32bf19dfcc89dabe412b074dc2c5f54d13714b
rodgers <rodgerstarrodgers@gmail.com>
2025-01-07 00:09:29 +0300

    maps return an array

--- map.js
+++ map.js
@@ -1,5 +1,12 @@
 [1, 2, 3, 4, 5, 6].map(number => console.log(number))
 
 // double array
+// maps loop and return an array
 let result = [1, 2, 3, 4, 5, 6].map(number => number * 2)
 console.log(result)
+
+const doubleMap = (numbers) => {
+    return numbers.map(number => number * 2)
+}
+
+console.log(doubleMap([5, 6, 8, 9, 7]))
