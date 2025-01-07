f4d655001ea16b10d0ce55ae5ddb8fd7f8c81577
rodgers <rodgerstarrodgers@gmail.com>
2025-01-08 02:38:12 +0300

    reduce

--- filters.js
+++ filters.js
@@ -11,8 +11,8 @@ const  filter = (numbers, greaterThan) => {
     return result
 }
 // console.log(filter([1, 2, 3, 4, 5, 6, 7, 8, 9], 5))
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-console.log(nums.filter(num => num >= 5 || num < 3))
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(nums.filter(num => num >= 5 || num < 3))
 
 const actors = [
     {name: 'rodger', netWorth: 20000000},
@@ -22,5 +22,20 @@ const actors = [
 // console.log(actors.filter(actor => actor.netWorth > 5000))
 
 // reduce
-// sum all of the net worths
-const
+
+// const nums = [1, 2, 3, 4]
+// const results =nums.reduce(function (previousValue, next) {
+//     return previousValue + next
+// })
+// console.log(results)
+
+// can also be simplified further
+function multiply(a, b) {
+    return a * b
+}
+const nums = [1, 2, 3, 4]
+const reults = nums.reduce(multiply) // also be written as nums.reduce((a, b) => a * b) - multiply
+
+console.log(reults)
+
+
